2890cef2a447e1e4717c54700bfbb5d4c2f101d7
Kaptajn-git <85395293+Kaptajn-git@users.noreply.github.com>
2024-01-11 22:17:55 +0100

    Update buzzer.py

--- Python/Buzzer/buzzer.py
+++ Python/Buzzer/buzzer.py
@@ -5,35 +5,38 @@ import time
 #        -pin to GND
 #
 # https://pages.mtu.edu/~suits/notefreqs.html
-#
 
 tempo = 2
 tones = {
-# Play middle C scale
-    'c': 261, # C4
-    'd': 294, # D4
-    'e': 330, # E4
-    'f': 349, # F4
-    'g': 392, # G4
-    'a': 440, # A4
-    'b': 494, # B4
-    'C': 523, # C5
-    ' ': 0,   # Reset and Shut up!
-# Play high C scale
-#    'c': 523, # C5
-#    'd': 587, # D5
-#    'e': 659, # E5
-#    'f': 698, # F5
-#    'g': 784, # G5
-#    'a': 880, # A5
-#    'b': 988, # B5
-#    'C': 1046, # C6 Note not possible on ESP8266 since max PWM is 1023
-#    ' ': 0,   # Reset and Shut up!
+
+    'c': 131, # C3
+    'd': 147, # D3
+    'e': 165, # E3
+    'f': 175, # F3
+    'g': 196, # G3
+    'a': 220, # A3
+    'b': 247, # B3
+    'C': 262, # C4
+    'D': 294, # D4
+    'E': 330, # E4
+    'F': 349, # F4
+    'G': 392, # G4
+    'A': 440, # A4
+    'B': 494, # B4
+    '1': 523, # C5
+    '2': 587, # D5
+    '3': 659, # E5
+    '4': 698, # F5
+    '5': 784, # G5
+    '6': 880, # A5
+    '7': 988, # B5
+#    '8': 1046, # C6 Note not possible on ESP8266 since max PWM is 1023
+    ' ': 0,   # Shut up!
 }
 
 beeper = PWM(Pin(14, Pin.OUT), freq=440, duty=512)
-melody = 'cdefgabC'
-rhythm = [8, 8, 8, 8, 8, 8, 8, 8]
+melody = 'cdefgabCDEFGAB1234567'
+rhythm = [8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8]
 
 for tone, length in zip(melody, rhythm):
     beeper.freq(tones[tone])
